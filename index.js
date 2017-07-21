46a9266387de41c0e573760363c7edc1452e1d9d
Alex Beregszaszi <alex@rtfs.hu>
2017-07-21 20:27:15 +0100

    Improve comments

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 class AbstractContainer {
-  // the constructor is given an instance of the kernel
+  // The constructor is given an instance of the kernel
   // https://github.com/primea/js-primea-hypervisor/blob/master/docs/kernel.md
   constructor (kernel) {
     this.kernel = kernel
   }
 
-  // this method runs once when the container is initially created. It is given
-  // a message with a single port, which is a channel to its parent with the
-  // exception of the root container (the container that is intailial created)
+  // This method runs once when the container is initially created.
+  //
+  // In general, a root instance receives no message, but every other instance
+  // receives a message with a singel port, which is a channel to its parent (aka the root).
+  //
+  // Optionally it can return a single value.
   initialize (message) {
     throw new Error('Unimplemented initialiser')
   }
 
-  // the function is called for each message that a container gets
+  // This is called for each incoming message the container gets.
+  //
+  // Optionally it can return a single value.
   run (message) {
     throw new Error('Unimplemented message: ' + message.data)
   }
 
+  // onIdle is called when there are no more immediate messages pending
+  // in the incoming queue for the container to receive.
+  //
+  // This can be overriden by a subclass.
   onIdle () {
+    // This tells the kernel that it is safe to freeze (aka turn off) this container.
+    // This removes all references in memory and the next time this instance is
+    // requested it has to be deserialised from storage and spinned up. This can be
+    // a time consuming operation.
     this.kernel.shutdown()
   }
 }
